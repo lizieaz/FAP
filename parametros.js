2dfa1762e0101a52cc43c2afcc1d380231674c36
lizieaz <139813498+lizieaz@users.noreply.github.com>
2023-08-10 12:27:36 +0000

    atualizacao

--- parametros.js
+++ parametros.js
@@ -1,7 +1,37 @@
-let nomeDoParametro = 'valorPadrao'
-
-function exibirSaudacao(nome, saudacao='Hello') {
+//1 - Crie uma função saudação para exibir Olá, [nome]
+// Valores padrões (nomeDoParamentro = 'Valor padrão')
+function exibirSaudacao(nome, saudacao='Olá') {
     console.log(`${saudacao}, ${nome}`);
 }
+//exibirSaudacao('Anelise');
 
-exibirSaudacao
+//2 - Função calculadora que receba 2 números e um operador como parâmetro e apresente o resultado
+function calculadora(num1, num2, operacao='+') {
+    switch (operacao) {
+        case '+':
+            console.log(`O resultado da operação é: ${num1 + num2}`);
+            break;
+        case '-':
+            console.log(`O resultado da operação é: ${num1 - num2}`);
+            break;
+        case '*':
+            console.log(`O resultado da operação é: ${num1 * num2}`);
+            break;
+        case '/':
+            console.log(`O resultado da operação é: ${num1 / num2}`);
+            break;
+        default:
+            break;
+    }
+}
+
+//calculadora(3,2);
+
+//3 - Função contagemRegressiva com número padrão de inicio
+function contagemRegressiva(inicio=10) {
+    while (inicio>0) {
+        console.log(`Contagem Regressiva: ${inicio} `);
+        inicio--
+    }
+}
+contagemRegressiva(10);

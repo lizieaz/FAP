91eeabccf3eb70e78f5837cee08e3400f81388a4
lizieaz <139813498+lizieaz@users.noreply.github.com>
2023-08-22 14:21:31 +0000

    exemplos

---
+++ Arrays/Objetos/objetos.js
@@ -0,0 +1,21 @@
+// const saudacao = function () {
+//     console.log("Olá, mundo!")
+// };
+//saudacao();
+
+//Criando objetos
+const cliente = {
+    nome: "Abc",
+    idade: 18,
+    endereco: "Rua s/n"
+};
+
+const pessoa = new Object();
+pessoa.nome = "Abc";
+pessoa.idade = 20;
+pessoa.endereco = "Rua longe";
+
+console.log(cliente);
+console.log(pessoa);
+
+console.log(pessoa.nome);
